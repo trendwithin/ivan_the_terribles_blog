db4cb39961702805bbd6d5a86e957085ee7dc8f7
Michael Becco <e4e6d4d5@gmail.com>
2015-04-02 08:42:37 -0700

    Mofify Config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,6 @@ IvanTheTerriblesBlog::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.cache_store = :redis_store, ENV['REDISTOGO_URL']
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
