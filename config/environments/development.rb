fbd2c16f17730d8c2fabb8fff19b7b7cbb6d73aa
Michael Becco <e4e6d4d5@gmail.com>
2015-04-02 08:27:03 -0700

    Add Redis To Config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ IvanTheTerriblesBlog::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6789'
+  config.cache_store = :redis_store, ENV['REDISTOGO_URL']
 end
